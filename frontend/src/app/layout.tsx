e82c3e9ffd7598249fbaac8b2b23a679c6f4cd5b
Maged Ibrahim <magedibra@yahoo.com>
2023-07-11 17:43:14 +0300

    Add Navbar component and prose className

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.scss";
+import Navbar from "./sections/Navbar/Navbar";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -14,7 +15,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="prose">
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
